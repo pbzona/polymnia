68fb5e46de893efe9b0f5f3148afb572c740aeb4
Phil Zona <phil.b.zona@gmail.com>
2021-03-22 14:20:43 -0500

    Add getComplement method to Color

--- src/Color.js
+++ src/Color.js
@@ -1,5 +1,10 @@
 const convert = require('color-convert')
 
+// Constants for controlling hue wrap, limiting sat and val changes
+const HUE_LIMIT = 360
+const SATURATION_LIMIT = 100
+const VALUE_LIMIT = 100
+
 class Color {
   constructor(color) {
     // For now, we assume the input color is a hex code (with or without hash sign)
@@ -29,8 +34,14 @@ class Color {
     this._onUpdate()
   }
 
+  getComplement() {
+    const complement = new this.constructor(this._color)
+    complement.adjustHue(HUE_LIMIT / 2)
+    return complement
+  }
+
   _adjustHue(amt) {
-    this._hue = (this._hue + amt) % 360
+    this._hue = (this._hue + amt) % HUE_LIMIT
     if (this._hue < 0) {
       this._hue = 360 - Math.abs(this._hue)
     }
@@ -42,8 +53,8 @@ class Color {
     this._saturation += amt
     if (this._saturation < 0) {
       this._saturation = 0
-    } else if (this._saturation > 100) {
-      this._saturation = 100
+    } else if (this._saturation > SATURATION_LIMIT) {
+      this._saturation = SATURATION_LIMIT
     }
 
     this._onUpdate()
@@ -53,8 +64,8 @@ class Color {
     this._value += amt
     if (this._value < 0) {
       this._value = 0
-    } else if (this._value > 100) {
-      this._value = 100
+    } else if (this._value > VALUE_LIMIT) {
+      this._value = VALUE_LIMIT
     }
 
     this._onUpdate()
