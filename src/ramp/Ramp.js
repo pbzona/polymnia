ea92e0952e3d6a8d0f46efc4b38a4108ca10410f
Phil Zona <phil.b.zona@gmail.com>
2021-03-24 15:23:29 -0500

    Add clone method to ramp

--- src/ramp/Ramp.js
+++ src/ramp/Ramp.js
@@ -36,6 +36,12 @@ class Ramp {
       color.adjustValue(amt)
     })
   }
+
+  clone() {
+    // https://stackoverflow.com/questions/41474986/how-to-clone-a-javascript-es6-class-instance
+    // Is this really the best way to deep clone?
+    return Object.assign(Object.create(Object.getPrototypeOf(this)), this)
+  }
 }
 
 module.exports = Ramp
