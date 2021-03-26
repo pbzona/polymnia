209a35eeb117677ad4da9632bd9e9c12c8e26c5c
Phil Zona <phil.b.zona@gmail.com>
2021-03-26 08:42:55 -0500

    Update examples in index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const Color = require('./src/Color')
+const LinearRamp = require('./src/ramp/LinearRamp')
 const MidpointRamp = require('./src/ramp/MidpointRamp')
 const Exporter = require('./src/Exporter')
 
-const c = new Color('296385')
-const r = new MidpointRamp(9, c)
+// MidpointRamp example
+const b = new Color('2189cf')
+const m = new MidpointRamp(9, b)
+m.hueStepSize = 15
+m.saturationStepSize = 10
+m.valueStepSize = 10
+m.reverseHueShift = true
+m.apply()
 
-r.hueStepSize = 15
-r.saturationStepSize = 8
-r.valueStepSize = 10
-r.reverseHueShift = true
-r.apply()
+m.elements.forEach(el => console.log(el._hsv))
 
-r.elements.forEach(el => console.log(el._hsv)) // Debug
-
-const e = new Exporter(32, r, 'output.png')
+const e = new Exporter(32, m, 'output.png')
 e.exportPNG()
+
+// LinearRamp example
+const c = new Color('c4c278')
+const d = new Color('6e3623')
+const l = new LinearRamp(6, c, d)
+l.apply()
+
+l.elements.forEach(el => console.log(el._hsv))
+
+const f = new Exporter(32, l, 'output_linear.png')
+f.exportPNG()
